20bbb4706e3c7aa4001aa8cdd4871108c011571b
Rushabh1134 <rushabhpatil557@gmail.com>
2024-07-20 22:39:19 +0530

    feat: Add react-icons package and update layout component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,10 @@
-import { Link } from "lucide-react";
 import { Button } from "./ui/button";
+import { FaDiscord, FaGithub, FaLinkedin, FaTwitter, FaX } from "react-icons/fa6";
 import Image from "next/image";
 
 import React from "react";
 import { CardContainer, CardBody, CardItem } from "./ui/3d-card";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -13,13 +14,25 @@ const Hero = () => {
           <h1 className="text-3xl sm:text-4xl lg:text-5xl">
             👋 Hi, I&apos;m Rushabh Patil.{" "}
           </h1>
-          <p className="text-lg sm:text-xl lg:text-2xl">
-            A people-first Product Designer with a passion for crafting
-            delightful experiences across screens. With a keen eye for clean
-            design, I am dedicated to creating elegant and visually appealing
-            user experiences that elevate complex workflows.
+          <p className="text-lg sm:text-xl lg:text-2xl mt-8">
+            I'm passionate about exploring the intersection of technology and innovation, and I'm actively involved in projects that challenge and expand my understanding of computer systems and software development. Looking forward to connecting and sharing insights with fellow enthusiasts in the field!
           </p>
-          <Button>Resume</Button>
+          <Button className="mt-8">Resume</Button>
+          <div className="flex gap-8 mt-8">
+            <Link href={""}>
+              <FaGithub style={{ fontSize: "25px" }} />
+            </Link>
+            <Link href={""}>
+              <FaLinkedin style={{ fontSize: "25px" }} />
+            </Link>
+            <Link href={""}>
+              <FaDiscord style={{ fontSize: "25px" }} />
+            </Link>
+            <Link href={""}>
+              <FaTwitter style={{ fontSize: "25px" }} />
+            </Link>
+
+          </div>
         </div>
         <CardContainer className="inter-var">
           <CardBody className=" relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]  dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-9 border  ">
